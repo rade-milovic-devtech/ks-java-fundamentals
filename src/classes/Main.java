b4bf577c1d54c7e9de4e2c3904b4b736678533cb
Nenad Mirkovic <nneennaadd@gmail.com>
2016-10-09 18:36:16 +0200

    Added exception & classes samples

---
+++ src/classes/Main.java
@@ -0,0 +1,80 @@
+package classes;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Person person1 = new Person();
+//        person1.name = "Nenad";
+//        person1.age = 27L;
+
+//        Person person2 = new Person("Rade",26L);
+
+//        Person person1 = new Person();
+//        person1.setName("Nenad");
+//        Person person2 = new Person();
+//        person2.setName("Nenad");
+
+//        System.out.println(person1.toString());
+//        System.out.println(person2.toString());
+
+//        System.out.println(person1.hashCode());
+//        System.out.println(person2.hashCode());
+
+//        if(person1.equals(person2)){
+//            System.out.println("Equals Its a same person");
+//        }else{
+//            System.out.println("Equals Its not a same person");
+//        }
+
+//        if(person1 == person2){
+//            System.out.println("== Its a same person");
+//        }else{
+//            System.out.println("== Its not a same person");
+//        }
+
+//        String string1 = "String";
+//        String string2 = "String";
+//
+//        if(string1 == string2){
+//            System.out.println("== Its a same string");
+//        }else{
+//            System.out.println("== Its not a same string");
+//        }
+
+//        Person person = new Person();
+//        person.setName("Nenad");
+
+//        modifyPerson(person);
+//        reAssignPerson(person);
+//        System.out.println(person.getName());
+
+//        String string = "someString";
+//        modifyString(string);
+//        System.out.println(string);
+
+//        int number = 36;
+//        modifyInt(number);
+//        System.out.println(number);
+    }
+
+
+
+//    private static void reAssignPerson(Person personA) {
+//        Person personB = new Person("Rade", 26L);
+//        personA = personB;
+//    }
+
+//    private static void modifyPerson(Person person1) {
+//        person1.setName("ModifiedNenad");
+//    }
+
+//    private static void modifyString(String string){
+//        string = "modifiedString";
+//    }
+
+//    private static void modifyInt(int number) {
+//        number = 99;
+//    }
+
+}
